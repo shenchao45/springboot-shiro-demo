66e86d2f196b61e5dd6700c7d1ba676cdc37c348
shenchao <shenchao@shouqianba.cn>
2018-03-20 14:41:23 +0800

    test 2 edit

--- src/main/java/com/shenchao/springbootshirodemo/service/UserService.java
+++ src/main/java/com/shenchao/springbootshirodemo/service/UserService.java
@@ -3,6 +3,5 @@ package com.shenchao.springbootshirodemo.service;
 import com.shenchao.springbootshirodemo.model.User;
 
 public interface UserService {
-//    test2 edit
     User findByUsername(String username);
 }
